6377504f555a7cf05ecb8f50285f85d538c965bb
sirprice <carljohan.dahlman@gmail.com>
2016-10-12 14:52:49 +0200

    senaste

--- Business/BusinessFacade.cs
+++ Business/BusinessFacade.cs
@@ -1,14 +1,12 @@
+using System.Threading.Tasks;
+using community.Models;
+using Microsoft.AspNetCore.Http;
+
 namespace community.Business
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class BusinessFacade
-    {
+    {   
         
-
-
-
-
-
-
     }
 }
--- Business/BusinessLogic.cs
+++ Business/BusinessLogic.cs
@@ -1,13 +1,12 @@
+using community.Models;
+using Microsoft.AspNetCore.Identity;
+
 namespace community.Business
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class BusinessLogic
     {
         
-
-
-
-
 
     }
 }
