09785064666e3992d469eb53c8cec0b101bf992b
sirprice <carljohan.dahlman@gmail.com>
2016-10-13 16:46:44 +0200

    asdf

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -10,10 +10,10 @@ namespace community.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        
-        public virtual List<GroupDB> Groups {get;set;}
-        public virtual List<MessageDB> ReceivedMessages {get;set;}
-        public virtual List<MessageDB> SentMessages {get;set;}
-        
+
+        public virtual List<GroupDB> Groups { get; set; }
+        public virtual List<MessageDB> ReceivedMessages { get; set; }
+        public virtual List<MessageDB> SentMessages { get; set; }
+
     }
 }
