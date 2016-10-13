b2c872af4697b4fdb1d4c22533159b0f3baf2b76
magnus-1 <magnus-1@users.noreply.github.com>
2016-10-13 14:07:34 +0200

    todo

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -29,6 +29,9 @@ namespace community.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            // TODO: builder.Entity<outstuff>.hasOne(k => till vad) . hasOneToMany
+
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
