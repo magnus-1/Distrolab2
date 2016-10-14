a4490643162d2835869c3a627bee1cf6e0617bcc
magnus-1 <magnus-1@users.noreply.github.com>
2016-10-14 15:27:36 +0200

    List functions, map, filter , reduce

--- DBLayer/DBModelConverter.cs
+++ DBLayer/DBModelConverter.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using community.ListUtils;
 using community.Models.BusinessModels;
 using community.Models.DBModels;
 
@@ -7,40 +8,29 @@ namespace community.DBLayer
 
     public class DBModelConverter
     {
-        public static List<MessageDB> ConvertMessageListBL(List<MessageBL> msgList)
-        {
-            List<MessageDB> dbl = new List<MessageDB>();
-
-
-            msgList.ForEach(p => dbl.Add(new MessageDB {  Content = p.Content }));
-            
-
-            System.Console.WriteLine("ConvertMessageListBL: " + dbl.ToString());
-            return dbl;
-        }
-
-        public static List<MessageBL> ConvertToMessageListBL(List<MessageDB> msgList)
-        {
-            List<MessageBL> dbl = new List<MessageBL>();
-
-            msgList.ForEach(p => dbl.Add(new MessageBL {  Content = p.Content }));
-            
-            System.Console.WriteLine("ConvertToMessageListBL: " + dbl.ToString());
-            return dbl;
-        }
-
         public static MessageDB ConvertMessageBL(MessageBL msg)
         {
-            return new MessageDB {  Content = msg.Content };
+            return new MessageDB { Content = msg.Content };
         }
         public static GroupDB ConvertGroupBL(GroupBL groupBL)
         {
-            return new GroupDB { Id = groupBL.Id, Title = groupBL.Title, Messages = ConvertMessageListBL(groupBL.Messages) };
+            return new GroupDB
+            {
+                Id = groupBL.Id,
+                Title = groupBL.Title,
+                Messages = ListConverter.Map(groupBL.Messages, m => new MessageDB{Content = m.Content})
+            };
         }
 
-        public static GroupBL ConvertToGroupBL(GroupDB groupDB) {
-            return new GroupBL{Title = groupDB.Title, Id = groupDB.Id, Messages = ConvertToMessageListBL(groupDB.Messages)};
+        public static GroupBL ConvertToGroupBL(GroupDB groupDB)
+        {
+            return new GroupBL
+            {
+                Title = groupDB.Title,
+                Id = groupDB.Id,
+                Messages = ListConverter.Map(groupDB.Messages, m => new MessageBL{Content = m.Content})
+            };
         }
-        
+
     }
 }
